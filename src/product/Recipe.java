9618959b321ccaef1edc3c60b25df1c22e7ac7a3
ITevgeniydav2022 <defox2022@gmail.com>
2022-12-28 14:13:15 +0300

    Сделано ДЗ Коллекции ч.2 Задания 1,2,3,4,5,6,7

---
+++ src/product/Recipe.java
@@ -0,0 +1,53 @@
+package product;
+
+import java.util.Objects;
+
+public class Recipe {
+
+    private ProductList productList;
+    private int totalCost;
+    private String recipeName;
+
+    public Recipe(ProductList productList, int totalCost, String recipeName) {
+        setProductList(productList);
+        setTotalCost(totalCost);
+        setRecipeName(recipeName);
+    }
+
+    public ProductList getProductList() {
+        return productList;
+    }
+
+    public void setProductList(ProductList productList) {
+        this.productList = productList;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(int totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Recipe recipe = (Recipe) o;
+        return Objects.equals(recipeName, recipe.recipeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recipeName);
+    }
+}
